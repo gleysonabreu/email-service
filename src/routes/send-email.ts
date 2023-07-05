9e4b82c3c59f2a0b411e347741960a92e7ef714c
Gleyson Abreu <gleyson_datu@hotmail.com>
2023-07-05 12:09:00 -0300

    feat: add simple validation for send mail

--- src/routes/send-email.ts
+++ src/routes/send-email.ts
@@ -5,6 +5,19 @@ import { sendEmailController } from '../use-cases/send-email';
 export async function sendEmailRoutes(server: FastifyInstance) {
   server.post(
     '/email',
+    {
+      schema: {
+        body: {
+          type: 'object',
+          required: ['to', 'password', 'passwordEncrypted'],
+          properties: {
+            to: { type: 'string' },
+            password: { type: 'string' },
+            passwordEncrypted: { type: 'string' },
+          },
+        },
+      },
+    },
     async (
       request: FastifyRequest<{
         Body: {
