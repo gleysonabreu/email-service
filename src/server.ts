b69b11a9217f1b9420b3a0efd1cbbd2a8ff3e7a1
Gleyson Abreu <gleyson_datu@hotmail.com>
2023-07-07 09:37:10 -0300

    feat: add cors

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,18 @@
 import fastify from 'fastify';
 
+import cors, { type FastifyCorsOptions } from '@fastify/cors';
+
 import { sendEmailRoutes } from './routes/send-email';
 
 function buildServer() {
   const server = fastify();
+
+  const corsOptions: FastifyCorsOptions = {
+    credentials: true,
+    origin: process.env.CORS_ORIGIN,
+  };
+
+  server.register(cors, corsOptions);
 
   server.get('/', async (_request, _reply) => {
     return 'hello world';
