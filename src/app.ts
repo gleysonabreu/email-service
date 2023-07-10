ae1d7bdb746082042d0ce54bfcb1cdfb7c8f8fe6
Gleyson Abreu <gleyson_datu@hotmail.com>
2023-07-10 20:09:03 -0300

    add config to deploy on render

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,13 @@
 import { config } from './config/env';
 import buildServer from './server';
 
+const host = 'RENDER' in process.env ? `0.0.0.0` : `localhost`;
+
 const server = buildServer();
 
 export const main = async () => {
   try {
-    await server.listen({ port: config.PORT });
+    await server.listen({ host, port: config.PORT });
 
     console.log(`Server listening on http://localhost:${config.PORT}`);
   } catch (error) {
